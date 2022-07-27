2c955e3480f6c3d20f766776bce26dcdcc901eaf
Evgenio89 <evgenio891@gmail.com>
2022-07-27 17:50:57 +0300

    todo 2

---
+++ src/Todolist.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {FilterValueType} from "./App";
+
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
+    removeTask: (taskID: number) => void
+    changeFilter: (value: FilterValueType) => void
+}
+
+export function Todolist(props: PropsType) {
+    return <div>
+        <h3>{props.title}</h3>
+        <div>
+            <input/>
+            <button>+</button>
+        </div>
+        <ul>
+            {props.tasks.map((task) => {
+                return (
+                    <li key={task.id}>
+                        <input type="checkbox" checked={task.isDone}/>
+                        <span>{task.title}</span>
+                        <button onClick={() => {
+                            props.removeTask(task.id)
+                        }}>x</button>
+                    </li>
+                )
+            })}
+
+        </ul>
+        <div>
+            <button onClick={() => props.changeFilter('all')}>All</button>
+            <button onClick={() => props.changeFilter('active')}>Active</button>
+            <button onClick={() => props.changeFilter('completed')}>Completed</button>
+        </div>
+    </div>
+}
